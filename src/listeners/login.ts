c168ccf5e309129b00174cfaf0d6a362de5bba3a
Huan LI <zixia@zixia.net>
2017-11-12 22:39:20 +0800

    get rid of <> because wechat get rid of it as html tag

--- src/listeners/login.ts
+++ src/listeners/login.ts
@@ -9,7 +9,7 @@ import {
 }               from '../config'
 
 async function onLogin(this: Sayable, user: Contact): Promise<void> {
-  const msg = `Der! I just got online, please show me the face! WechatyBlinder<v${blinder.version()}>`
+  const msg = `Der! I just got online, please show me the face! WechatyBlinder v${blinder.version()}`
 
   log.info('Bot', msg)
   await announce(msg)
