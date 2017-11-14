27ae34645bd2c6e7e07ce5c83a4310e42e5b22d7
Huan LI <zixia@zixia.net>
2017-11-14 13:51:57 +0800

    disable upgrade by default

--- src/brain.ts
+++ src/brain.ts
@@ -27,7 +27,8 @@ export class Brain {
     const duration = (Date.now() - timeStart) / 1000
     log.info('Brain', 'start() blinder initialized, cost %s seconds', duration.toFixed(0))
 
-    await blinder.updateEmbeddingStore()
+    // Only need to upgrade the FlashStore data when upgrade from version under v0.5.11
+    // await blinder.updateEmbeddingStore()
 
     const runner = new Promise<void>((resolve, reject) => {
       bot.once('stop', resolve)
